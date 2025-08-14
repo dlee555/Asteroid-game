385f9c4fe945b3958595bd8a1c93a3a02a5e6b59
dlee555 <Yeong.MingLee1999@outlook.com>
2025-08-13 22:04:55 -0700

    updated asteroid game, added shooting ability and asteroids

--- asteroid-game/main.py
+++ asteroid-game/main.py
@@ -1,9 +1,13 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
@@ -14,15 +18,34 @@ def main():
     timeclock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots,updatable,drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable,drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game Over!")
+                sys.exit()
+
         screen.fill(color="black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
         dt = timeclock.tick(60)/1000
 
